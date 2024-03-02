1af1bef9781c802400ea30f122a46b74a3f59d02
Ezra Erewharighe <kelvinerewharighe@gmail.com>
2024-03-02 21:05:54 +0100

    Updated...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [			
+    AppComponent,
+      DashboardComponent,
+      LoginComponent,
+      LayoutComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
