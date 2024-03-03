6dafe1b76ced624ca2b3138f3ec79cf6eb87529f
Ezra Erewharighe <kelvinerewharighe@gmail.com>
2024-03-03 20:52:07 +0100

    Updated...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { LayoutComponent } from './layout/layout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'login',
+    pathMatch:'full'
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'',
+    component:LayoutComponent,
+    children: [
+      {
+        path:'dashboard',
+        component:DashboardComponent
+      }
+    ]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { LayoutComponent } from './layout/layout.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [			
@@ -13,10 +15,11 @@ import { LayoutComponent } from './layout/layout.component';
       DashboardComponent,
       LoginComponent,
       LayoutComponent
-   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+    ],
+    imports: [
+      BrowserModule,
+      AppRoutingModule,
+      FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
