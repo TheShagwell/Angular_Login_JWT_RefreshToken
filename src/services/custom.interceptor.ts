877e4ad01e5cf99e287d065ea70b2c939e576f62
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2024-04-05 05:25:51 +0100

    Updated...

--- src/services/custom.interceptor.ts
+++ src/services/custom.interceptor.ts
@@ -1,5 +1,17 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const customInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+
+  let loggedUserData : any;
+  const localDataStorage = localStorage.getItem('userData'); // in order to convert to object from local storage
+  if (localDataStorage!= null) {
+    loggedUserData = JSON.parse(localDataStorage);
+  }
+
+  const cloneRequest = req.clone({
+    setHeaders: {
+      Authorization: `Bearer ${loggedUserData.token}`  
+    }
+  })
+  return next(cloneRequest);
 };
